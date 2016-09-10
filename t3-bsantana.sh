9ebfc7baba4395c147455c3f1c8d01e3ca713d4e
brendasalenave <brendasalenave@live.com>
2016-09-10 15:47:44 -0300

    dns - ok dhcp - ????

--- t3-bsantana.sh
+++ t3-bsantana.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
 
-array=(${array[*]} `sed '/^#/ d' $1 | cut -f1,2 -d' '`)
-#for i in ${array[@]} ; do echo -e "$i" ; done
-
 field1=(${field1[*]} `sed '/^#/ d' $1 | cut -f1 -d' '`)
 field2=(${field2[*]} `sed '/^#/ d' $1 | cut -f2 -d' '`)
 field3=(${field3[*]} `sed '/^#/ d' $1 | cut -f3 -d' '`)
 field4=(${field4[*]} `sed '/^#/ d' $1 | cut -f4 -d' '`)
+field5=(${field5[*]} `sed '/^#/ d' $1 | cut -f5 -d' '`)
 
-for i in ${!field1[@]}; do echo -e "=${field1[$i]}.inf.ufsm.br:${field2[$i]}" ; done > dns.conf
+echo -e "#
+# Delegated nameserver records (someone else provides the SOA)
+#
+#   &fqdn:ip:x:ttl =>
+#
+#	NS record x.ns.fqdn as nameserver for fqdn
+#	A record mapping x.ns.fqdn -> ip [if ip present]
+&10.in-addr.arpa::dns-int.inf.ufsm.br.
+&42.18.200.in-addr.arpa::dns-int.inf.ufsm.br.
+cd&inf.ufsm.br::dns-int.inf.ufsm.br.
+#" > dns.conf
+
+for i in ${!field1[@]}; do
+  echo -e "=${field1[$i]}.inf.ufsm.br:${field2[$i]}"
+  if [[ ${field5[$i]} != "-" ]] ; then
+    echo -e "C${field5[$i]}.inf.ufsm.br:${field1[$i]}.inf.ufsm.br"
+  fi
+done >> dns.conf
 
 ########################################################################## DHCPD ######################################################################################
+
+echo -e "####################
+## Global options ##
+####################
+
+not authoritative;" > dhcpd.conf
+
+
 
 g=(${g[*]} `sed -n -e '/^# [A-Z|h]/ d' -e '/^# [a-z]/p' $1`)
 groups=(${groups[*]} `echo ${g[*]/"#"}`)
