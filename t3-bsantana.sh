65d9c20e48a85663cc5feaf2a523d6d41885df32
brendasalenave <brendasalenave@live.com>
2016-09-11 14:46:46 -0300

    foi ???

--- t3-bsantana.sh
+++ t3-bsantana.sh
@@ -5,6 +5,7 @@ field2=(${field2[*]} `sed '/^#/ d' $1 | cut -f2 -d' '`)
 field3=(${field3[*]} `sed '/^#/ d' $1 | cut -f3 -d' '`)
 field4=(${field4[*]} `sed '/^#/ d' $1 | cut -f4 -d' '`)
 field5=(${field5[*]} `sed '/^#/ d' $1 | cut -f5 -d' '`)
+f2=(${f2[*]} `sed '/^#/ d' $1 | cut -f2 -d' ' | cut -f1,2,3 -d'.' | sort -n |uniq`)
 
 echo -e "#
 # Delegated nameserver records (someone else provides the SOA)
@@ -27,11 +28,24 @@ done >> dns.conf
 
 ########################################################################## DHCPD ######################################################################################
 
-echo -e "####################
+echo -e "
+####################
 ## Global options ##
 ####################
 
-not authoritative;" > dhcpd.conf
+not authoritative;
+
+option domain-name "inf.ufsm.br";
+option domain-name-servers 200.18.42.3;
+option subnet-mask 255.255.255.0;
+
+default-lease-time 7200;
+max-lease-time 7200;
+
+ddns-update-style none;
+
+shared-network inf-ufsm-br {
+# internal IPs" > dhcpd.conf
 
 
 g=(${g[*]} `sed -n -e '/^# [A-Z|h]/ d' -e '/^# [a-z]/p' $1`)
@@ -43,18 +57,36 @@ groups=(${groups[*]} `echo ${g[*]/"#"}`)
 #for i in ${groups[@]} ; do
 
 for ((u = 0; u < ${#f2[@]}; u++)) ; do
-  echo -e "\tsubnet ${f2[$u]}.0 netmask 255.255.255.0\n\t{"
+  echo -e "\tsubnet ${f2[$u]}.0 netmask 255.255.255.0\t{"
+  if [[ $u == 0 ]] ; then
+    echo -e "\t\tnot authoritative;\n\t\toption routers 10.1.1.1;\n\t\toption broadcast-address 10.1.1.255;\n\t\toption subnet-mask 255.255.255.0;\n\t\toption domain-name-servers 200.18.42.3;\n"
+  else
+    echo -e "\t\tauthoritative;\n\t\toption routers 200.18.42.7;\n\t\toption broadcast-address 200.18.42.255;\n\t\toption subnet-mask 255.255.255.0;\n"
+  fi
   for ((i = 0; i < ${#groups[@]}; i++)) ; do
-    echo -e "\t\tgroup {"
+    #echo -e "\t\t# group ${groups[$i]}"
+    echo -ne "\t\tgroup {"
     for ((j = 0; j < ${#field1[@]}; j++)) ; do
-      #echo -e ""${field4[$j]}" == "${groups[$i]}" && "${field2[$j]}" =~ "${f2[$u]}""
       if [[ "${field4[$j]}" == "${groups[$i]}" && "${field2[$j]}" =~ "${f2[$u]}" ]]; then
-          echo -e "\t\t\thost ${field1[$j]} { hardware ethernet ${field3[$j]}; fixed-address ${field2[$j]}; }"
-      fi
+          #echo -ne "\n\t\t\thost ${field1[$j]} { hardware ethernet ${field3[$j]}; fixed-address ${field2[$j]}; }"
+
+      ###
+      f3=(`echo ${field3[$j]} | sed 's/,/ /g' | cut -f1,2,3 -d' '`)
+      for ((k = 0; k < ${#f3[@]}; k++)) ; do
+        if [[ $k == 0 ]] ; then
+          echo -ne "\n\t\t\thost ${field1[$j]} { hardware ethernet ${field3[$j]}; fixed-address ${field2[$j]}; }"
+        else
+          echo -ne "\n\t\t\thost ${field1[$j]}$k { hardware ethernet ${field3[$j]}; fixed-address ${field2[$j]}; }"
+        fi
+      done
+    fi
+      ##
     done
-    echo -e "\t\t}"
+    echo -e " }\r"
   done
   echo -e "\t}"
-done
+done >> dhcpd.conf
+echo "}" >> dhcpd.conf
 
+sed -i -e 's/group { }//g' -e 's/} }/}\n\t\t}/g' -e '/^\s*$/d' -e '/^$/d' dhcpd.conf
 echo "{$field3[*]}" | cut -f3 -d' ' | cut -f1,2,3,4,5,6,7,8,9,10 -d','
