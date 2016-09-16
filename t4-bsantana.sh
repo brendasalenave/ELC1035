0013b0dfec03a714ae902f52616f6043b9d29167
brendasalenave <brendasalenave@live.com>
2016-09-16 10:49:58 -0300

    select

--- t4-bsantana.sh
+++ t4-bsantana.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+0#!/bin/bash
 OUTPUT="/tmp/input.txt"
 >$OUTPUT
 
@@ -16,24 +16,66 @@ mostraMenu(){
   0 "Exit" 2>temp
 }
 
-findMachine(){
+selectMachine2edit(){
+  field1=(${field1[*]} `sed '/^#/ d' $1 | cut -f1 -d' '`)
+  COUNTER=1
+  RADIOLIST=""  # variable where we will keep the list entries for radiolist dialog
+  for i in "${field1[@]}"; do
+    RADIOLIST="$RADIOLIST $COUNTER $i off "
+    let COUNTER=COUNTER+1
+  done
+
+  ESCOLHA=$(dialog --title "Select machine to edit" \
+  --stdout \
+  --radiolist "" 0 0 $COUNTER \
+  $RADIOLIST)
+  dialog --title "Máquinas:" --msgbox "$ESCOLHA" 7 30
+
+ }
+
+select_machine(){
+  field1=(${field1[*]} `sed '/^#/ d' $1 | cut -f1 -d' '`)
+  COUNTER=1
+  RADIOLIST=""  # variable where we will keep the list entries for radiolist dialog
+  for i in "${field1[@]}"; do
+    RADIOLIST="$RADIOLIST $COUNTER $i off "
+    let COUNTER=COUNTER+1
+  done
+
+  ESCOLHA=$(dialog --title "Select machine to edit" \
+          --stdout \
+          --checklist "" 0 0 $COUNTER \
+          $RADIOLIST)
+
+  dialog --title "Máquinas:" --msgbox "$ESCOLHA" 7 30
+}
+
+confirm(){
+  dialog --title "  Are u sure?" \
+  --yes-label "yes" \
+  --no-label "NO" \
+  --yesno "Removing an item, it will be lost permanently." 7 33
+}
+
+searchMachine(){
   # show an inputbox
   dialog --title "Inputbox - To take input from you" \
   --inputbox "Machine" 8 60 2>$OUTPUT
 
   # get respose
-  respose=$?
+  _ret=$?
   # get data stored in $OUPUT using input redirection
   name=$(<$OUTPUT)
   echo -e "\n\n$name"
 
   # make a decsion
-  case $respose in
+  case $_ret in
     0)
       echo "entrou aqui"
       echo "$name"
       m=`sed -n -e "/$name/p" $1`
-      dialog --title "Machines Found" --msgbox "$m" 100 100
+      echo $m
+      dialog --title "Machines Found" --msgbox "$m" 100 80
       ;;
     1)
       echo "Cancel pressed."
@@ -41,6 +83,39 @@ findMachine(){
     255)
      echo "[ESC] key pressed."
   esac
+}
+
+add_machine(){
+  dialog --title "Hostname" \
+  --inputbox "" 8 60 2>$OUTPUT
+
+  _aux=$(<$OUTPUT)
+  _str="$_aux"
+
+  dialog --title "IP" \
+  --inputbox "" 8 60 2>$OUTPUT
+  _aux=$(<$OUTPUT)
+  _str="$_str $_aux"
+
+  dialog --title "MAC" \
+  --inputbox "" 8 60 2>$OUTPUT
+  _aux=$(<$OUTPUT)
+  _str="$_str $_aux"
+
+  dialog --title "Group" \
+  --inputbox "" 8 60 2>$OUTPUT
+  _aux=$(<$OUTPUT)
+  _str="$_str $_aux"
+
+  dialog --title "Alias" \
+  --inputbox "" 8 60 2>$OUTPUT
+  _aux=$(<$OUTPUT)
+  if [[ "$_aux" == "" ]] ; then
+    _str="$_str -"
+  else _str="$_str $_aux"
+  fi
+
+  echo $_str >> $1
 }
 
 #while : ; do
@@ -51,54 +126,22 @@ findMachine(){
 
     case $choice in
             1)
-                echo -e "You chose Option 1"
-                #findMachine
-                # show an inputbox
-                dialog --title "Inputbox - To take input from you" \
-                --inputbox "Machine" 8 60 2>$OUTPUT
+                echo -e "You chose Option 1: Search machine"
+                searchMachine $1 ;;
 
-                # get respose
-                respose=$?
-                # get data stored in $OUPUT using input redirection
-                name=$(<$OUTPUT)
-                echo -e "\n\n$name"
-
-                # make a decsion
-                case $respose in
-                  0)
-                    echo "entrou aqui"
-                    echo "$name"
-                    m=`sed -n -e "/$name/p" $1`
-                    dialog --title "Machines Found" --msgbox "$m" 100 100
-                    ;;
-                  1)
-                    echo "Cancel pressed."
-                    ;;
-                  255)
-                   echo "[ESC] key pressed."
-                esac
-
-                ;;
             2)
-                echo "You chose Option 2"
-                dialog                                         \
-                --title 'Parabéns'                             \
-                --msgbox 'Instalação finalizada com sucesso.'  \
-                6 40
-                ;;
+                echo "You chose Option 2: Add machine"
+                add_machine $1 ;;
             3)
-                echo -e "You chose Option 3"
-                field1=(${field1[*]} `cat $1 | cut -f1 -d' '`)
-                dialog                                         \
-                --title 'Select machine(s) to delete'             \
-                --msgbox 'Instalação finalizada com sucesso.'  \
-                6 40
+                echo -e "You chose Option 3: Delete machine"
+                select_machine $1
+                confirm
                 ;;
             4)
-                echo -e "You chose Option 4"
-                ;;
+                echo -e "You chose Option 4: Edit machine"
+                selectMachine2edit $1 ;;
             5)
-                echo -e "You chose Option 5"
+                echo -e "You chose Option 5: View machine"
                 dialog --title "List file" --msgbox "$(cat $1)" 100 100
                 ;;
             0) clear
@@ -106,6 +149,7 @@ findMachine(){
     esac
   # Cancel is pressed
   else
+    clear
     echo "Cancel is pressed"
   fi
 #done
