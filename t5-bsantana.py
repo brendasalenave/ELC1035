1a161f59663668fc69d181a587e81067f672ad0a
brendasalenave <brendasalenave@live.com>
2016-09-25 13:35:24 -0300

    nada util

--- t5-bsantana.py
+++ t5-bsantana.py
@@ -2,20 +2,21 @@
 
 import os, requests, bs4
 
+#url = 'http://phdcomics.com'
 url = 'http://xkcd.com'
-os.makedirs('xkcd', exist_ok=True)
+os.makedirs('phd', exist_ok=True)
 print('Downling page %s...'% url)
 res=requests.get(url)
 res.raise_for_status()
 
-soup=bs4.BeautifulSoup(res.text)
+soup=bs4.BeautifulSoup(res.text,"lxml")
 
 for fig in soup.select('#comic img'):
     comicUrl='http:' + fig.get('src')
     print('Downloading image %s...' % (comicUrl))
     res=requests.get(comicUrl)
     res.raise_for_status()
-    imgFile=open(os.path.join('xkcd',
+    imgFile=open(os.path.join('phd',
     os.path.basename(comicUrl)),'wb')
     for chunk in res.iter_content(100000):
         imgFile.write(chunk)
