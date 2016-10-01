0bde723b770e9d8b7b8973cf50a6e54d9a706ffa
brendasalenave <brendasalenave@live.com>
2016-10-01 20:46:22 -0300

    feito!

--- t5-bsantana.py
+++ t5-bsantana.py
@@ -3,7 +3,6 @@
 import os, requests, bs4, re
 
 url = 'http://phdcomics.com'
-#url = 'http://xkcd.com'
 os.makedirs('phd', exist_ok=True)
 print('Downloading page %s...'% url)
 res=requests.get(url)
@@ -13,7 +12,6 @@ soup=bs4.BeautifulSoup(res.text,"lxml")
 
 for fig in soup.find_all('img', src=re.compile('http://www.phdcomics.com/comics/archive/' + 'phd.*gif$')):
     comicUrl=fig.get('src')
-    #print('Downloading image %s...' % (comicUrl))
     res=requests.get(comicUrl)
     res.raise_for_status()
     imgFile=open(os.path.join('phd',
@@ -24,14 +22,15 @@ for fig in soup.find_all('img', src=re.compile('http://www.phdcomics.com/comics/
 
 id_ = re.findall(r'\d+', comicUrl)
 id_fig = int(id_[0])
+limit = id_fig
 
 count = 0
 for u in range(50):
     id_fig = id_fig - 1
     comicUrl = 'http://www.phdcomics.com/comics/archive/phd0' + str(id_fig) + 's.gif'
     res=requests.get(comicUrl)
-    if (count >= 7): break
-    if(res.status_code != 404) and (count < 4):
+    if (count >= 5): break
+    if(res.status_code != 404) and (count < 5):
         res.raise_for_status()
         imgFile=open(os.path.join('phd',
         os.path.basename(comicUrl)),'wb')
@@ -39,17 +38,14 @@ for u in range(50):
             imgFile.write(chunk)
         imgFile.close()
         count += 1
-    else: id_fig = id_fig + 1
-
-
 
 imgs = os.listdir('phd')
-print(len(imgs))
 if (len(imgs) > 5):
+    a = sorted(imgs)
+    imgs = a[::-1]
     t = len(imgs)
-    for i in os.walk('phd'):
-        for u in range(5, t):
-            os.remove(imgs[u])
-        break
+    os.chdir('phd')
+    for u in range(5,t):
+        os.remove(imgs[u])
 
 print('Done!')
